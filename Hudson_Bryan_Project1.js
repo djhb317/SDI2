02c014d47a69a74dd1fb29728f305ea2d7d153fc
Bryan Hudson <hbinupt@yahoo.com>
2012-05-09 01:29:39 -0400

    Boolean Availability

--- Hudson_Bryan_Project1.js
+++ Hudson_Bryan_Project1.js
@@ -1,7 +1,16 @@
 alert("let's get show ready.")
+//boolean availabilty
+
+var totalGear = function (available) {
+    if (totalGear = true)
+    console.log("Your gear is available.");
+    else if (totalGear = false)
+    console.log("Sorry.  The gear you requested in not avilable.");
+};
 //count gear loop
 for (var totalGear = 0; totalGear < 47; totalGear++) {
     console.log("You have selected" + totalGear + "pieces for this show out of 47.")
 };
   console.log("Your gear is ready for the show!")  
     
+;
