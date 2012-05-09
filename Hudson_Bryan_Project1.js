52e738acf2f2eb1e694ee84f4d7f5136fdec4e45
Bryan Hudson <hbinupt@yahoo.com>
2012-05-09 00:39:54 -0400

    Show prep gear count - for loop

---
+++ Hudson_Bryan_Project1.js
@@ -0,0 +1,7 @@
+alert("let's get show ready.")
+//count gear loop
+for (var totalGear = 0; totalGear < 47; totalGear++) {
+    console.log("You have selected" + totalGear + "pieces for this show out of 47.")
+};
+  console.log("Your gear is ready for the show!")  
+    
