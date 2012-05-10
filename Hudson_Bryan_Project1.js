c6ac4b5ffeea9d106852f02b43a96d29b7902e5f
Bryan Hudson <hbinupt@yahoo.com>
2012-05-10 02:28:30 -0400

    Array time

--- Hudson_Bryan_Project1.js
+++ Hudson_Bryan_Project1.js
@@ -12,5 +12,20 @@ for (var totalGear = 0; totalGear < 47; totalGear++) {
     console.log("You have selected" + totalGear + "pieces for this show out of 47.")
 };
   console.log("Your gear is ready for the show!")  
-    
 ;
+//show setup and exicution array
+var runShow = [ setUp, preShow, show, showBreakDown];
+    var setUp = 6;
+    var preShow = 2;
+    var show = 5;
+    var showBreakDown = 5;
+    console.log("Setup for this show is"  + setUp +  "hours.");
+    console.log("Pre Show prep for this show is"  + preShow +  "hours.");
+    console.log("The run time for this show is"  + show +  "hours.");
+    console.log("The show break down for this show is"  + showBreakDown +  "hours.");
+
+
+
+    
+
+    
