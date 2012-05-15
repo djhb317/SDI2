fa30f9af70408a06e5a4a026ba043367a8e65100
Bryan Hudson <hbinupt@yahoo.com>
2012-05-15 02:01:36 -0400

    syntax error fix

--- Hudson_Bryan_Assignment2.js
+++ Hudson_Bryan_Assignment2.js
@@ -1,18 +1,20 @@
-alert("let's get show ready.")
+alert("let's get show ready.");
 //boolean availabilty
 var totalGear = function (available) {
-    if (totalGear = true)
+    if (totalGear = true){
+    };
     console.log("Your gear is available.");
-    else if (totalGear = false)
+    else if (totalGear = false){
+    };
     console.log("Sorry.  The gear you requested in not avilable.");
 };
 
 //count gear loop
 for (var totalGear = 0; totalGear < 47; totalGear++) {
-    console.log("You have selected" + totalGear + "pieces for this show out of 47.")
+    console.log("You have selected" + totalGear + "pieces for this show out of 47.");
 };
-  console.log("Your gear is ready for the show!")  
-;
+  console.log("Your gear is ready for the show!");  
+
 
 //show setup and run through array
 var runShow = [ setUp, preShow, show, showBreakDown];
